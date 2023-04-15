a2d3e4cb50d263308621e3e81519ed2d2477e5ca
danilo maldovan <danilo.central@gmail.com>
2023-04-15 12:35:57 -0300

    restructuracion

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import { Navbar } from "./components/Navbar/Navbar";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Contacto } from "./components/Contacto/Contacto";
+import { Nosotros } from "./components/Nosotros/Nosotros";
 import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
 import { CartProvider } from "./components/CartContext/CartContext";
-import { Cart } from "./components/Cart/Cart";
-import { Footer } from "./components/Footer/Footer";
+import { Cart } from "./Cart/Cart";
+// import { Footer } from "./components/Footer/Footer";
+
 
 function App() {
 
@@ -20,10 +23,12 @@ return (
           <Route path="/productos/:categoryId" element={ <ItemListContainer /> }/>
           <Route path="/detail/:itemId" element={ <ItemDetailContainer/> }/>
           <Route path="/cart" element={ <Cart/>} />
+          <Route path="/nosotros" element={ <Nosotros /> }/>
+          <Route path="/contacto" element={ <Contacto /> }/>
           <Route path="*" element={<Navigate to="/" /> }/>
         </Routes>
     
-          {<Footer/>}
+          {/* <Footer/>   */}
       </BrowserRouter>
     </CartProvider>
   );
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*import { Link, useNavigate } from "react-router-dom"*/
 import { useContext, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
@@ -35,7 +34,7 @@ export const ItemDetail = ({item}) => {
             <img src={item.img} alt={item.name} />
             <p>{item.description} </p>
             <p>Precio: ${item.price}</p>
-            {item.stock <= 5 && <p><strong>Quedan solo {item.stock} unidades! </strong></p>}
+            {item.stock <= 5 && <p><strong>Ultimas unidades disponibles!</strong></p>}
 
 
         {
@@ -54,65 +53,7 @@ export const ItemDetail = ({item}) => {
     
             <br/>
             <button onClick={handleVolver} className="btn btn-primary">Volver</button>
-=======
-import { useContext, useMemo, useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import { CartContext } from "../../context/CartContext"
-// import ColorPicker from "../../ejemplos/ColorPicker/ColorPicker"
-import ItemCount from "../ItemCount/ItemCount"
-import LowStockMsg from "./LowStockMsg"
-import Memo from "../Memo"
 
-
-const ItemDetail = ({item}) => {
-    const { agregarAlCarrito, isInCart } = useContext(CartContext)
-   
-    const [cantidad, setCantidad] = useState(1)
-    const navigate = useNavigate()
-    
-    const handleVolver = () => {
-        navigate(-1)
-    }
->>>>>>> f6f788b7457cca58d552a05610af058cd6085122
-
-    const handleAgregar = () => {
-        const newItem = {
-            ...item,
-            cantidad
-        }
-
-        agregarAlCarrito(newItem)
-    }
-
-    const fechaMontaje = useMemo(() => new Date().toLocaleString(), [])
-
-    return (
-        <div className="container my-5">
-            <h2>{item.name}</h2>
-            <img src={item.img} alt={item.name}/>
-            <p>{item.description}</p>
-            <p>Precio: ${item.price}</p>
-            {/* <ColorPicker setColor={setColor} options={colores}/> */}
-            { item.stock <= 5 && <LowStockMsg stock={item.stock}/>}
-
-            <p>{fechaMontaje}</p>
-            {/* <Memo /> */}
-
-            {
-                isInCart(item.id)
-                    ?   <Link to="/cart" className="btn btn-success my-2">Terminar mi compra</Link>
-                    :   <ItemCount 
-                            max={item.stock}
-                            cantidad={cantidad}
-                            setCantidad={setCantidad}
-                            handleAgregar={handleAgregar}
-                        />
-            }
-
-            <br/>
-            <button onClick={handleVolver} className="btn btn-primary">Volver</button>
         </div>
     )
 }
-
-export default ItemDetail
